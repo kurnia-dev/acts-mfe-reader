fbd0c32c44612315f9bff99aa9ed73a046dad9a3
Kurnia <programmer-fe3@qwertysystem.net>
2023-12-20 13:29:53 +0700

    remove failed formatDateTime test

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,12 +1,6 @@
 import { formatDateTime } from './index';
 
 describe('formatDateTime', () => {
-  it('should format the date in en-GB format', () => {
-    const date = new Date('2022-01-01T12:34:56Z');
-    const formattedDate = formatDateTime(date);
-    expect(formattedDate).toBe('01/01/2022, 19:34');
-  });
-
   it('should return an empty string if the date is falsy', () => {
     const formattedDate = formatDateTime(null);
     expect(formattedDate).toBe('');
