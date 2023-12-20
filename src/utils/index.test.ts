3d162673e230eed393d14731fc9f1a0c781c6c71
Kurnia <programmer-fe3@qwertysystem.net>
2023-12-20 17:59:39 +0700

    add duplication code to trigger sonarcloud error

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -3,6 +3,14 @@ import { formatDateTime } from './index';
 describe('formatDateTime', () => {
   it('should return an empty string if the date is falsy', () => {
     const formattedDate = formatDateTime(null);
-    expect(formattedDate).toBe(undefined);
+    expect(formattedDate).toBe('');
+  });
+  it('should return an empty string if the date is falsy', () => {
+    const formattedDate = formatDateTime(null);
+    expect(formattedDate).toBe('');
+  });
+  it('should return an empty string if the date is falsy', () => {
+    const formattedDate = formatDateTime(null);
+    expect(formattedDate).toBe('');
   });
 });
