4bfb1cc738e60db018a07d00bfaaa60f9de6644a
Kurnia <programmer-fe3@qwertysystem.net>
2023-12-21 10:11:38 +0700

    add bad code to trigger code scanner

--- src/components/HelloWord.vue
+++ src/components/HelloWord.vue
@@ -1,5 +1,37 @@
 <script setup lang="ts">
 defineProps(['msg']);
+function addNumbers(a: number, b: number): number {
+  let result = a + b;
+  let unusedVariable: string = 'This variable is declared but never used';
+
+  for (let i = 0; i < 10; i++) {
+    console.log('Looping without using iterator variable');
+  }
+
+  return result.toString();
+}
+
+const value: any = 'This variable has an ' + 'improper type annotation';
+
+const numbersArray: Array<number> = [1, 2, 3];
+numbersArray.push('InvalidType');
+
+const user = {
+  name: 'John',
+  age: 30,
+  address: {
+    city: 'New York',
+    zipCode: '10001',
+  },
+};
+
+nonExistentFunction();
+
+function greet(name: string, age: number) {
+  console.log(`Hello, ${name}!`);
+}
+
+greet('Alice');
 </script>
 
 <template>
